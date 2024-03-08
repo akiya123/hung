58bcbc956eb6194553d7392448230409adcce134
nhu-hoan <162651637+nhu-hoan@users.noreply.github.com>
2024-03-08 17:34:07 +0700

    ok

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Mẹ';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Mẹ';
+const contentCard = 'Nhân ngày 8/3 con chúc mẹ ngày càng mạnh khoẻ , vui vẻ , tạm gác lại những âu lo để tận hưởng cuộc sống nhé ❤️❤️❤️'
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
